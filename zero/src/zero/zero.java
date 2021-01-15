7a6a914a3b6e3987d7cb0aca0e429c537482aeac
59013-28-18 <59013-28-18@STA6017528.exchange.ad.afpanet>
2021-01-15 08:55:32 +0100

    new one

--- zero/src/zero/zero.java
+++ zero/src/zero/zero.java
@@ -23,16 +23,14 @@ public class zero {
 		
 		/*int[][] A =  {{3,5,3,1,5},{5,7,5,5,5},{7,8,9,2,4},{5,5,2,6,6},{5,5,5,5,5}};
 		
-		int i , j,n,m;
+		int i ,j,n,m;
 		n=5;
 		
 		
 		for (i=0; i<n; i++)
-			for (j=0;j<n;j++) {
-		
-				if(i==j || (i+j+1)==n )
-					A[i][j]=0;
-				
+			 {
+					A[i][i]=0;
+					A[i][n-1-i]=0;
 			}
 		
 		
@@ -40,10 +38,6 @@ public class zero {
 			for (j=0;j<n;j++)
 				System.out.print(A[i][j] + "   ");
 		System.out.println();
-		}
-
-		*/
-		
+		}*/	
 	}
-
 }
